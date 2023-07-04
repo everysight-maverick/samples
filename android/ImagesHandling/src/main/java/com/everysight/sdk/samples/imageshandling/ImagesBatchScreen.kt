86608ca5bf8364dc426343147b0f02654f6fce10
Ben Amsallem <benchuk@gmail.com>
2023-07-04 11:17:44 +0300

    update api changes

--- android/ImagesHandling/src/main/java/com/everysight/sdk/samples/imageshandling/ImagesBatchScreen.kt
+++ android/ImagesHandling/src/main/java/com/everysight/sdk/samples/imageshandling/ImagesBatchScreen.kt
@@ -1,13 +1,11 @@
 package com.everysight.sdk.samples.imageshandling
 
 import UIKit.app.Screen
-import UIKit.app.data.*
 import UIKit.app.resources.Font
 import UIKit.app.resources.ImgSrc
 import UIKit.app.resources.UIResource
 import UIKit.widgets.Image
 import UIKit.widgets.Text
-import UIKit.widgets.UIResourceElement
 
 class ImagesBatchScreen:Screen() {
 
@@ -17,17 +15,12 @@ class ImagesBatchScreen:Screen() {
         super.onCreate()
         text.setResource(Font.StockFont.Small).setCenter(getWidth()/2).setY(getHeight()/3)
         add(text)
-
-        //your can upload custom fonts as well via the Text UI element
+        //you can upload custom fonts as well via the Text UI element
         requestResourcesUpload(screenImages)
-
-    }
-
-    override fun onResourcesUploadStart() {
-        super.onResourcesUploadStart()
         text.setVisibility(true)
         text.setText("Loading...")
     }
+
     override fun onResourceUploadResult(resource: UIResource) {
         text.setText("${resource.getResourceName()} was loaded")
     }
@@ -38,7 +31,6 @@ class ImagesBatchScreen:Screen() {
         screenImages.forEachIndexed{index,imgSrc->
             add(Image().setResource(imgSrc).setX(index*50f).setY(getHeight()/2))
         }
-
     }
 
     companion object{
@@ -51,5 +43,4 @@ class ImagesBatchScreen:Screen() {
             screenImages.add(ImgSrc("orange.jpg",ImgSrc.Slot.s5))
         }
     }
-
 }
